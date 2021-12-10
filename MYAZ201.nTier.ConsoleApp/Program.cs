fcdba81e1a5739e15a339c21c8570e904d8f379c
Zafer CÖMERT <zcomert@samsun.edu.tr>
2021-12-10 14:50:47 +0300

    Add project files.

---
+++ MYAZ201.nTier.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MYAZ201.nTier.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
