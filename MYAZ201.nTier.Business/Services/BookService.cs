a0050da77d39c31f12ad1d045d93b513f46688fb
Zafer CÖMERT <zafer.comert@hotmail.com>
2021-12-19 21:04:13 +0300

    BookService has been implemented considering Constructor Dependency Injection.

---
+++ MYAZ201.nTier.Business/Services/BookService.cs
@@ -0,0 +1,47 @@
+﻿using MYAZ201.nTier.Business.Interfaces;
+using MYAZ201.nTier.DAL.Abstract;
+using MYAZ201.nTier.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MYAZ201.nTier.Business.Services
+{
+    public class BookService : IBookService
+    {
+        private readonly IBookDal _bookDal;
+
+        public BookService(IBookDal bookDal)
+        {
+            _bookDal = bookDal;
+        }
+
+        public void Add(Book entity)
+        {
+            _bookDal.Add(entity);
+        }
+
+        public void Delete(Book entity)
+        {
+            _bookDal.Delete(entity);
+        }
+
+        public Book Get(Expression<Func<Book, bool>> filter)
+        {
+            return _bookDal.Get(filter);
+        }
+
+        public List<Book> GetAll(Expression<Func<Book, bool>> filter = null)
+        {
+            return _bookDal.GetAll(filter);
+        }
+
+        public void Update(Book entity)
+        {
+            _bookDal.Update(entity);
+        }
+    }
+}
