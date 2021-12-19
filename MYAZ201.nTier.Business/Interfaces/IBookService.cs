23caceb2ff633f74f07d5a7bf4876995a0d26a2a
Zafer CÖMERT <zafer.comert@hotmail.com>
2021-12-19 21:01:18 +0300

    IService<T> has been implemented.

---
+++ MYAZ201.nTier.Business/Interfaces/IBookService.cs
@@ -0,0 +1,9 @@
+﻿using MYAZ201.nTier.DAL.Entities;
+
+namespace MYAZ201.nTier.Business.Interfaces
+{
+    public interface IBookService : IService<Book>
+    {
+
+    }
+}
---
+++ MYAZ201.nTier.Business/Interfaces/IService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MYAZ201.nTier.Business.Interfaces
+{
+    public interface IService<T>    
+        where T : class, new()
+    {
+        void Add(T entity);
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
